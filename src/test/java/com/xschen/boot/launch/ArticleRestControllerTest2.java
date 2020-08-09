08a94f3a3bdf18a25accb7eb67f799c732c4596b
xschen <xschen@trip.com>
2020-08-09 19:31:34 +0800

    Servlet 容器环境下的测试

--- src/test/java/com/xschen/boot/launch/ArticleRestControllerTest2.java
+++ src/test/java/com/xschen/boot/launch/ArticleRestControllerTest2.java
@@ -1,38 +1,40 @@
 package com.xschen.boot.launch;
 
-import com.xschen.boot.launch.controller.ArticleController;
 import lombok.extern.slf4j.Slf4j;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpMethod;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import javax.annotation.Resource;
 
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 
 /**
  * @author xschen
+ *
+ * SpringBootTest：用来创建Spring的上下文ApplicationContext，保证测试在上下文环境里运行。
+ * 单独使用SpringBootTest不会启动Servlet容器，所以不能使用@Resource和@Autowired等注解进行bean的注入
  */
 
+
 @Slf4j
-public class ArticleRestControllerTest {
+@AutoConfigureMockMvc // 表示mockMvc对象由Spring依赖注入构建，只负责使用就可以了
+@SpringBootTest
+public class ArticleRestControllerTest2 {
 
-    //mock对象, 模拟发送http请求
-    private static MockMvc mockMvc;
+    @Resource
+    private MockMvc mockMvc;
 
-    //在所有测试方法执行之前进行mock对象初始化
-    @BeforeAll
-    static void setUp() {
-        mockMvc = MockMvcBuilders.standaloneSetup(new ArticleController()).build();
-    }
-
-    //测试方法
     @Test
     public void saveArticle() throws Exception {
-
         String article = "{\n" +
                 "    \"id\": 1,\n" +
                 "    \"author\": \"zimug\",\n" +
@@ -41,20 +43,21 @@ public class ArticleRestControllerTest {
                 "    \"createTime\": \"2017-07-16 05:23:34\",\n" +
                 "    \"reader\":[{\"name\":\"zimug\",\"age\":18},{\"name\":\"kobe\",\"age\":37}]\n" +
                 "}";
-        // 模拟http请求
-        MvcResult result = mockMvc.perform(
+        ResultActions resultActions = mockMvc.perform(
                 MockMvcRequestBuilders
-                        .request(HttpMethod.POST, "/rest/articles")
-                        .contentType("application/json;charset=utf-8")
-                        .content(article)
-        )
-                // 如果不满足执行结果就会失败
+                .request(HttpMethod.POST, "/rest/articles")
+                .contentType("application/json")
+                .content(article)
+        );
+        resultActions.andReturn().getResponse().setCharacterEncoding("utf-8");
+        MvcResult mvcResult = resultActions
                 .andExpect(MockMvcResultMatchers.status().isOk())  //HTTP:status 200
                 .andExpect(MockMvcResultMatchers.jsonPath("$.data.author").value("zimug"))
                 .andExpect(MockMvcResultMatchers.jsonPath("$.data.reader[0].age").value(18))
                 .andDo(print())
                 .andReturn();
-        result.getResponse().setCharacterEncoding("utf-8");
-        System.out.println(result.getResponse().getContentAsString());
+
+        log.info(mvcResult.getResponse().getContentAsString());
+
     }
 }
